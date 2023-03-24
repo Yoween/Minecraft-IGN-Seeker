722c4a18dae352fef2c7ae14690ea07006dd1c47
Yoween <larochetristan17@gmail.com>
2023-03-24 18:45:50 +0100

    fixed wrong error handling

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
 class Main:
     def __init__(self, file: str, startPos: int, timeout = 1):
         #Init counter
-        if startPos == "":
-            startPos = 0
-        count = startPos
-        word_file = open(file, "r")
-        word_array = []
-        for word in word_file:
-            if len(word) > 3:
-                word_array.append(word.strip())
-        word_file.close()
-        start = time.time() #start timer
-        # Loop for each word
-        len_array = len(word_array)
-        headers = {'Content-Type': 'application/json'}
-        while count < len_array:
-            difference = len_array - count
-            payload = []
-            for i in range(difference if difference < 10 else 10):
-                payload.append(word_array[count+i])
-            request = requests.post("https://api.mojang.com/profiles/minecraft", headers = headers, data = json.dumps(payload))
-            response = request.json()
-            if request.status_code == 429:
-                count -= difference
-                time.sleep(600)
-            else:
-                len_response = len(response)
-                payload_sorted = sorted(payload)
-                payload_index = 0
-                content_index = 0
-                while payload_index < len_response:
-                    payload_item = payload_sorted[payload_index]
-                    if payload_item != response[content_index]["name"].lower():
-                        print(f"{payload_item} is available (#{count+payload_index})")
-                        unclaimed_file = open("unclaimed.txt", "a")
-                        unclaimed_file.write(payload_item + "\n")
-                        unclaimed_file.close()
-                        content_index-=1
-                    else:
-                        print(f"{payload_item} is already used (#{count+payload_index})")
-                        claimed_file = open("claimed.txt", "a")
-                        claimed_file.write(payload_item + "\n")
-                        claimed_file.close()
-                    payload_index+=1
-                    content_index+=1
-            count += 10
-            time.sleep(timeout)
-        end = time.time()
-        print(f"Done! ({end-start} seconds)")
-        input()
+        try:
+            if startPos == "":
+                startPos = 0
+            count = int(startPos)
+            word_file = open(file, "r")
+            word_array = []
+            invalid = False
+            for word in word_file:
+                if len(word) > 3:
+                    word_array.append(word.strip())
+                else:
+                    invalid = True
+            if invalid:
+                print("Some words are shorter than the minimum allowed by Mojang.\nIndexes may not be accurate to the provided list\nBUT WILL BE for the program if you want to change the starting position.")
+                if input("Still want to continue? y/n: ") != "y":
+                    return
+            word_file.close()
+            start = time.time() #start timer
+            # Loop for each word
+            len_array = len(word_array)
+            headers = {'Content-Type': 'application/json'}
+            while count < len_array:
+                difference = len_array - count
+                payload = []
+                for i in range(difference if difference < 10 else 10):
+                    payload.append(word_array[count+i])
+                request = requests.post("https://api.mojang.com/profiles/minecraft", headers = headers, data = json.dumps(payload))
+                if request.status_code == 429:
+                    print("Timed out due to too many requests. Waiting a bit to go again")
+                    time.sleep(30)
+                else:
+                    response = request.json()
+                    payload_sorted = sorted(payload)
+                    payload_index = 0
+                    content_index = 0
+                    while payload_index < len(payload_sorted):
+                        payload_item = payload_sorted[payload_index]
+                        try:
+                            if payload_item != response[content_index]["name"].lower():
+                                print(f"{payload_item} is available (#{count+payload_index})")
+                                unclaimed_file = open("unclaimed.txt", "a")
+                                unclaimed_file.write(payload_item + "\n")
+                                unclaimed_file.close()
+                                content_index-=1
+                            else:
+                                print(f"{payload_item} is already used (#{count+payload_index})")
+                                claimed_file = open("claimed.txt", "a")
+                                claimed_file.write(payload_item + "\n")
+                                claimed_file.close()
+                        except:
+                            pass
+                        payload_index+=1
+                        content_index+=1
+                count += 10
+                time.sleep(timeout)
+            end = time.time()
+            print(f"Done! ({end-start} seconds)")
+            input()
+        except Exception as error:
+            print("Unexpected error:", error)
 
 class WindowUI: 
     def __init__(self):
@@ -62,6 +74,7 @@ class WindowUI:
         startPosEntry.pack()
 
         v1 = DoubleVar()
+        v1.set(1)
 
         timeoutText = Label(self.root, text = "Time to pause between actions (in seconds)")
         timeoutText.pack()
